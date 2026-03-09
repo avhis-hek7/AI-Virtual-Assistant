0c318269abf8234e212adfd07fd601957a01efe9
avhis-hek7 <sharmaavishek679@gmail.com>
2026-03-09 10:47:05 +0545

    get current user api work connected

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserDataContext from "./UserDataContext";
+import axios from "axios";
 
 function UserContext({ children }) {
   const serverUrl = "http://localhost:8000";
+
+  const [userData, setUserData] = useState(null);
+
+  const handleCurrentUser = async() => {
+    try {
+      const result = await axios.get(`${serverUrl}/api/user/currentuser`,{
+        withCredentials:true
+      })
+      setUserData(result.data);
+      console.log(result.data);
+    } catch (error) {
+      console.log(error)
+      
+    }
+  }
+
+  useEffect(()=>{
+    handleCurrentUser();
+  },[])
 
   return (
     <UserDataContext.Provider value={{ serverUrl }}>
